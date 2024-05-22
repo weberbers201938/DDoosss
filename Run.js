2134a540d999e8a0b6317320ec7e03be8f6a1cf5
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-05-22 10:37:51 +0800

    Create Run.js

---
+++ Run.js
@@ -0,0 +1,20 @@
+function startBot(message) {
+  if (message) logger(message, "[ Starting ]");
+
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Dos.js"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true,
+  });
+
+  child.on("close", codeExit => {
+    if (codeExit !== 0 || (global.countRestart && global.countRestart < 5)) {
+      global.countRestart = (global.countRestart || 0) + 1;
+      startBot("Restarting system...");
+    }
+  });
+
+  child.on("error", error => logger("An error occurred: " + JSON.stringify(error), "[ Starting ]"));
+}
+
+start();
